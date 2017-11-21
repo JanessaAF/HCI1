547210e1bbbf2ad5baf70c66891c7d00b030e6ee
JanessaAF <janessa.af@gmail.com>
2017-11-20 23:02:33 -0600

    Added course selection page Added cart functionality for course selection -no checkout page linking yet bug: tethering on popover is off:shift to the left if possible

---
+++ js/view.js
@@ -0,0 +1,44 @@
+var subjects = ["Agriculture", "Biology", "Computer Science", "English",
+"Film Studies", "Mathematics", "Physics", "Sociology", "Women's Studies"];
+
+var courses = {Agriculture: [{code:"AGRI1000", name: "Introduction to Agriculture", description: "An introduction to agriculture.", hours:"1:30PM-2:30PM", days:"MWF", instructor:"Dr. White", location:"EITC E2 105", credits:"3.00"}]}
+
+//show subjects tab
+function showSubjects(term){
+  var subjectsHtml = "";
+  $('#subjectColumn').show();
+  //handle which term is highlighted
+  $('#termColumn ul li a.active').removeClass("active");
+  $('#'+ term).addClass("active")
+  //build subjects list html
+  subjects.forEach(function(item){
+    itemNoSpace = item.indexOf("/\s/")!=-1 ? item.replace(/\s/g, "+") : item;
+    subjectsHtml += '\<li class="nav-item"\>\<a id="'+ itemNoSpace +'" class="nav-link" href="#'+ itemNoSpace +'" onclick="showCourses(\''+item+'\');"\>'+ item +'\</a\>\</li\>';
+  });
+  $("#subjects").html(subjectsHtml);
+  return subjectsHtml;
+}
+
+//show courses
+function showCourses(subject){
+  var course;
+  var coursesHtml = "\<h3\>Courses:\</h3\>";
+  console.log(subject);
+  $('#courseColumn').show();
+  //handle which subject is highlighted
+  $('#subjectColumn ul li a.active').removeClass("active");
+  $('#'+ subject).addClass("active")
+  //build course list html
+  courses[subject].forEach(function(item){
+    course = '\<div class="card"\>\<div class="card-header"\>\<h5\>'+ item.code + ' - ' + item.name +'\</h5\>\</div\>' //header
+    course += '\<div class="card-body"\> \<div class="row"\>\<div class="col-sm-8"\> \<div class="courseDesc"\>'+ item.description +'\</div\>';//body code
+    course += '\<div class="courseTime"\> Time: '+ item.days + " " + item.hours +'\</div\>\</div\>';
+    course += '\<div class="col-sm-4"\> \<div class="courseProf"\> Instructor: '+ item.instructor +'\</div\>';
+    course += '\<div class="courseLocation"\> Location: '+ item.location +'\</div\>';
+    course += '\<div class="courseCredits"\> Credits: '+ item.credits +'CR \</div\>\</div\>\</div\>\</div\>'
+    course += '\<div class="card-footer"\>\<button type="button" class="btn btn-info" onclick=addToCart();\>\<i class="fa fa-cart-plus" aria-hidden="true"\>\</i\> Add to Cart\</button\>\</div\>\</div\>';
+    coursesHtml += course;
+  });
+  $("#coursesColumn").html(coursesHtml);
+  return coursesHtml;
+}
